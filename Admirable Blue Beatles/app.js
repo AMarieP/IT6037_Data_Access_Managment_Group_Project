62be94a4b11190c3c102fdf2f7a118a735dd3866
A <alyssa@kvasl.com>
2023-11-24 18:25:09 +1300

    added new routes

--- Admirable Blue Beatles/app.js
+++ Admirable Blue Beatles/app.js
@@ -1,15 +1,12 @@
 import express from 'express'
 import  {connectDB, connectAtlasDB } from './db/connectdb.js'
-import { getAllData } from './models/schema.js'
+import getAllData from './utils/getAllData.js'
 import allRoutes from "./routes/route.js"
+import articleRoutes from "./routes/article.js"
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
-
-
 const app = express()
-//connect with env file other wise take the default value 
-const port =process.env.PORT||'3000'
+const port =process.env.PORT||'3000' //connect with env file other wise take the default value 
 const DATABASE_URL =process.env.DATABASE_URL|| 'mongodb+srv://20220756:20220756abb@admirablebluebeatles.ha32r6f.mongodb.net/'
 const DATABASE_URI=process.env.DATABASE_URI|| "mongodb+srv://20220756:20220756abb@admirablebluebeatles.ha32r6f.mongodb.net/?retryWrites=true&w=majority";
 const DATABASE_NAME="abb_db"
@@ -22,8 +19,10 @@ const DATABASE_NAME="abb_db"
 connectAtlasDB(DATABASE_URI)
 
 
-// routes will be localhost:300/data/{other routes }
-app.use("/",allRoutes)
+// routes will be localhost:3000/data/{other routes }
+app.use("/", allRoutes)
+app.use("/", articleRoutes)
+
 
 app.listen(port,()=>{
     console.log(`Server listening at http://localhost:${port}`)
