5b5357b88f522fd34cc5e6076438bf2933f7406e
A <alyssa@kvasl.com>
2023-11-24 18:24:12 +1300

    removed sep models

--- Admirable Blue Beatles/models/digitalResource.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import { subjectModel } from './subject';
-
-const digitalResourceSchema = mongoose.Schema({
-    subjects: [subjectModel]
-});
-
-const digitalResourceModel = mongoose.model('digitalResource', digitalResourceSchema)
-module.exports = digitalResourceModel;
