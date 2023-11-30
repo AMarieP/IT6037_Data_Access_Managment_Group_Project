a4f4ad2b0822f8b2bf3cda4c22ea6f3adf5369fe
A <alyssa@kvasl.com>
2023-11-30 15:50:03 +1300

    Check

--- Admirable Blue Beatles/db/connectdb.js
+++ Admirable Blue Beatles/db/connectdb.js
@@ -1,28 +1,28 @@
 import mongoose from "mongoose";
 
-//URI - Should be stored in env file lol 
-const uri = "mongodb+srv://20220756:20220756abb@admirablebluebeatles.ha32r6f.mongodb.net/abb_db?retryWrites=true&w=majority";
+// //URI - Should be stored in env file lol 
+// const uri = "mongodb+srv://20220756:20220756abb@admirablebluebeatles.ha32r6f.mongodb.net/abb_db?retryWrites=true&w=majority";
 
-//function opens connection with mongoose 
-export default function connectDB(cb) {
+// //function opens connection with mongoose 
+// export default function connectDB(cb) {
  
-  try {
-    mongoose.connect(uri, {
-    });
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-  const dbConnection = mongoose.connection;
-  dbConnection.once("open", (_) => {
-    console.log(`Database connected: ${uri}`);
-    return cb()
-  });
+//   try {
+//     mongoose.connect(uri, {
+//     });
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+//   const dbConnection = mongoose.connection;
+//   dbConnection.once("open", (_) => {
+//     console.log(`Database connected: ${uri}`);
+//     return cb()
+//   });
  
-  dbConnection.on("error", (err) => {
-    console.error(`connection error: ${err}`);
-    return cb(err)
+//   dbConnection.on("error", (err) => {
+//     console.error(`connection error: ${err}`);
+//     return cb(err)
 
-  });
-  return;
-}
+//   });
+//   return;
+// }
