836502d0d79543be9b1320acbe0e344866fa5058
A <alyssa@kvasl.com>
2023-12-07 17:07:37 +1300

    Fixed authentication issues

--- Admirable Blue Beatles/middlewears/auth.js
+++ Admirable Blue Beatles/middlewears/auth.js
@@ -2,24 +2,39 @@ import jwt from 'jsonwebtoken'
 import { User } from '../models/user.js'
 
 //Authenticates the Token for Sign In
-const authenticate = async(req, res, next) =>  {
-    if (req.session && req.session.user) {
-        // If there's a user in the session, it means the user is authenticated
-        next();
 
-    } else if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-        const deocodedToken = jwt.verify(req.headers.authorization.split(' ')[1], process.env.SECRET_KEY)
-        const ID = deocodedToken._id
-        const user = await User.findById(ID)
-        console.log(user)
+const authenticate = async(req, res, next) =>  {
+    
+    //Checks if session and session.user are truthy
+    if (req.session && req.session.user) {
+
+        //Gets token from session
+        const token = req.session.user.token
+
+        //Decodes token
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+
+        //Gets user id from token
+        const this_user_id = decodedToken._id
+
+        //Search DB for the user
+        const user = await User.findById(this_user_id)
+
+        //Check is user is truthy
         if(!user){
-            return res.status(404).json({message: "User Not Found!"})
+            //If false send error 404
+            return res.status(404).json({message: "User Not Found! Token is Invalid."})
         }else{
+            //Set the req.user to this user
             req.user = user;
+            //Run next function
             next()
         }
-    }else{
-        return res.status(404).json({message: "Error with Token!"})
+
+    }
+    //Handles errors
+    else{
+        return res.status(404).json({message: "Error with Token! Not Signed In!"})
     }
   };
 
